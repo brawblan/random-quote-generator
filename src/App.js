c25a0aee0aacdbca6fc9a34a7336ff049a67ae47
Brandon Blankenstein <blankensteinpercussion@gmail.com>
2021-01-26 18:45:09 -0600

    started FCC random-quote project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+
+const quotes = [
+	{ 
+		"quote" : "The only sin is ignorance", 
+		"author" : "Christopher Marlowe" 
+	},
+	{
+		"quote" : "A man is his own easiest dupe, for what he wishes to be true he generally believes to be true", 
+		"author" : "Demosthenes"
+	},
+	{
+		"quote" : "A lie can travel halfway around the world while the truth is putting on its shoes", 
+		"author" : "Mark Twain"
+	},
+	{
+		"quote" : "Great minds discuss ideas; average minds discuss events; small minds discuss people", 
+		"author" : "Eleanor Roosevelt"
+	},
+	{
+		"quote" : "If you have a garden and a library, you have everything you need", 
+		"author" : "Marcus Tullius Cicero"
+	},
+	{
+		"quote" : "Truth comes out in wine", 
+		"author" : "Pliny the Elder"
+	},
+	{
+		"quote" : "Everything in the universe is within you. Ask all from yourself", 
+		"author" : "Rumi"
+	},
+	{
+		"quote" : "When I get a little money I buy books; and if any is left I buy food and clothes", 
+		"author" : "Desiderius Erasmus"
+	}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="quote-box" className="box-wrapper col">
+      <div className="quote-author-wrapper col">
+        <div id="text" className="quote">
+        {quotes[0]['quote']}
+        </div>
+        <div id="author" className="author">
+          -{quotes[0]['author']}
+        </div>
+      </div>
+      <div className="btn-tweet-wrapper row">
+        <a href="#" id="tweet-quote" className="btn tweet-quote">Tweet</a>
+        <button id="new-quote" className="btn new-quote">New Quote</button>
+      </div>
     </div>
   );
 }
